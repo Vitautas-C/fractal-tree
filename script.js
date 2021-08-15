e3a82a43b941eb64119a815daad7e4d9cf998c6a
Vitautas-C <shadow7549@gmail.com>
2021-08-15 22:15:29 +0300

    add leaves and color controls

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const canvas = document.querySelector(".canvas")
 const ctx = canvas.getContext("2d")
+
 const controlWrapper = document.querySelector(".control-wrapper")
 
-let iterationCount, trunkHeight, trunkThickness, branchRatio, branchProbability, leafProbability, branchLengthVariability, forkAngle, forkAnglePull, branchRotationVariability, leafSize, branchColor, leafColor, leafColorVariability
+let iterationCount, trunkHeight, trunkThickness, branchRatio, branchProbability, leafProbability, branchLengthVariability, forkAngle, forkAnglePull, branchRotationVariability, leafSize, branchColor, leafColor, leafColorVariability, leafLightness
 
 
 const timers = []
@@ -21,7 +22,7 @@ controlWrapper.onchange = () => {
 
 
 function updateValues() {
-    ({ iterationCount, trunkHeight, trunkThickness, branchRatio, branchProbability, leafProbability, branchLengthVariability, forkAngle, forkAnglePull, branchRotationVariability, leafSize, branchColor, leafColor, leafColorVariability } = Object.fromEntries([...controlWrapper.querySelectorAll('[type="range"]')].map(({ id, value }) => [id, value])))
+    ({ iterationCount, trunkHeight, trunkThickness, branchRatio, branchProbability, leafProbability, branchLengthVariability, forkAngle, forkAnglePull, branchRotationVariability, leafSize, branchColor, leafColor, leafColorVariability, leafLightness, } = Object.fromEntries([...controlWrapper.querySelectorAll('[type="range"]')].map(({ id, value }) => [id, +value])))
 }
 
 
@@ -50,27 +51,37 @@ function clear() {
 
 
 function drawTree(x0, y0, angle, length, width, i) {
-    if (i < 7 || length < 8) {
-        if (rnd() < 0.05) return
-        const hue = 120 - rnd(40)
-        const saturation = 70 - rnd(30)
-        const lightness = 30 + rnd(20)
-        ctx.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`
-        ctx.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness - 25}%)`
-        drawLeaf(x0, y0, 4 + rnd(2), angle)
-        return
-    }
+
+    angle += rnd(branchRotationVariability) * (rnd() < 0.5 ? -1 : 1) * (iterationCount - i)
 
     const timerId = setTimeout(() => {
         const { x, y } = findEnd(x0, y0, angle, length)
         ctx.lineWidth = width
-        ctx.strokeStyle = `hsl(${80 - i * 3}, 60%, ${40 - i}%)`
+        ctx.strokeStyle = `hsl(${branchColor + (iterationCount - i) * 3}, 60%, ${30 - i}%)`
         drawLine(x0, y0, x, y)
+
+        if (i < 7 || length < 8) {
+            const hue = leafColor + rnd(leafColorVariability)
+            const saturation = 70 - rnd(30)
+            const lightness = 20 + leafLightness + rnd(20)
+            ctx.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+            ctx.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness - 25}%)`
+            drawLeaves(x0, y0, leafSize - rnd(leafSize / 2), angle, length)
+            return
+        }
+
         const hasLeftBranch = rnd() < branchProbability
         const hasRightBranch = rnd() < branchProbability
         width -= (width - (i == 8 ? 1 : 0.5)) / (i - 7)
         const nextForkAngle = 1 - rnd(1 - forkAnglePull)
-        if (hasLeftBranch) drawTree(x, y, angle - forkAngle / 2 * nextForkAngle, length * branchRatio * (1 + rnd(branchLengthVariability) * (rnd() < 0.5 ? -1 : 1)), width, i - 1)
+        if (hasLeftBranch) drawTree(
+            x,
+            y,
+            angle - forkAngle / 2 * nextForkAngle,
+            length * branchRatio * (1 + rnd(branchLengthVariability) * (rnd() < 0.5 ? -1 : 1)),
+            width,
+            i - 1
+        )
         if (hasRightBranch) drawTree(
             x,
             y,
@@ -97,6 +108,7 @@ function drawCircle(x, y, r) {
 
 
 function drawLeaf(x, y, r, angle) {
+    ({ x, y } = findEnd(x, y, angle, r))
     ctx.beginPath()
     ctx.ellipse(x, y, r, r / (rnd(3) + 1.5), angle, 0, 7)
     ctx.fill()
@@ -104,6 +116,16 @@ function drawLeaf(x, y, r, angle) {
     ctx.stroke()
 }
 
+function drawLeaves(x0, y0, r, angle, length) {
+    const { x: x1, y: y1 } = findEnd(x0, y0, angle, length)
+    const { x: x2, y: y2 } = findEnd(x0, y0, angle, length * 3 / 4)
+    const { x: x3, y: y3 } = findEnd(x0, y0, angle, length / 2)
+    if (rnd() < leafProbability) drawLeaf(x1, y1, r, angle)
+    if (rnd() < leafProbability) drawLeaf(x2, y2, r, angle - 1)
+    if (rnd() < leafProbability) drawLeaf(x2, y2, r, angle + 1)
+    if (rnd() < leafProbability) drawLeaf(x3, y3, r, angle - 1)
+    if (rnd() < leafProbability) drawLeaf(x3, y3, r, angle + 1)
+}
 
 function findEnd(beginX, beginY, angle, length) {
     return {
